858cf6068a283b423ae36955207469e41bae9575
Zachary Westerman <zacharywesterman@yahoo.com>
2017-01-25 04:43:48 -0600

    outputs all operators

--- CreateDLL/main.cpp
+++ CreateDLL/main.cpp
@@ -4,15 +4,18 @@
 
 #include "script/operators/add.h"
 #include "script/operators/subtract.h"
+#include "script/operators/multiply.h"
 
 __declspec(dllexport) void __cdecl get_opers_c(core::array< script::oper::oper_t<char>* >& output)
 {
     output.add(new script::oper::add<char>);
     output.add(new script::oper::subtract<char>);
+    output.add(new script::oper::multiply<char>);
 }
 
 __declspec(dllexport) void __cdecl get_opers_w(core::array< script::oper::oper_t<wchar_t>* >& output)
 {
     output.add(new script::oper::add<wchar_t>);
     output.add(new script::oper::subtract<wchar_t>);
+    output.add(new script::oper::multiply<wchar_t>);
 }

f19948893524c483b5dab786674af3731700783f
Zachary Westerman <zacharywesterman@yahoo.com>
2017-01-25 04:59:34 -0600

    added modulus operator

--- CreateDLL/main.cpp
+++ CreateDLL/main.cpp
@@ -4,21 +4,30 @@
 
 #include "script/operators/add.h"
 #include "script/operators/subtract.h"
+
 #include "script/operators/multiply.h"
 #include "script/operators/divide.h"
+#include "script/operators/integer_divide.h"
+#include "script/operators/modulus.h"
 
 __declspec(dllexport) void __cdecl get_opers_c(core::array< script::oper::oper_t<char>* >& output)
 {
     output.add(new script::oper::add<char>);
     output.add(new script::oper::subtract<char>);
+
     output.add(new script::oper::multiply<char>);
     output.add(new script::oper::divide<char>);
+    output.add(new script::oper::integer_divide<char>);
+    output.add(new script::oper::modulus<char>);
 }
 
 __declspec(dllexport) void __cdecl get_opers_w(core::array< script::oper::oper_t<wchar_t>* >& output)
 {
     output.add(new script::oper::add<wchar_t>);
     output.add(new script::oper::subtract<wchar_t>);
+
     output.add(new script::oper::multiply<wchar_t>);
     output.add(new script::oper::divide<wchar_t>);
+    output.add(new script::oper::integer_divide<wchar_t>);
+    output.add(new script::oper::modulus<wchar_t>);
 }
--- CreateDLL/script/operators/modulus.h
+++ CreateDLL/script/operators/modulus.h
@@ -17,8 +17,8 @@ namespace script
         class modulus : public oper_t<CHAR>
         {
         public:
-            ///Addition uses '+' and has a priority of 3.
-            modulus() : oper_t<CHAR>(core::string<char>("/"), 4) {}
+            ///Modulus uses '%' and has a priority of 4.
+            modulus() : oper_t<CHAR>(core::string<char>("%"), 4) {}
 
 
             ///Perform multiplication
@@ -26,7 +26,45 @@ namespace script
             {
                 error_flag operation_error = error::NONE;
 
-                /** Stuff goes here **/
+                core::string<CHAR> arg1;
+                core::string<CHAR> arg2;
+
+                if (operands.pop(arg2))
+                {
+                    if (operands.pop(arg1))
+                    {
+                        if (is_script_string(arg1) ||
+                            is_script_string(arg2))
+                        {
+                            operation_error |= error::INVALID_OPERATION;
+                        }
+                        else
+                        {
+                            double val1 = core::value(arg1);
+                            double val2 = core::value(arg2);
+
+                            if (val2 == 0.f)
+                            {
+                                operation_error |= error::DIV_BY_ZERO;
+                                operands.push(0.f);
+                            }
+                            else
+                            {
+                                double result = (double)((long int)val1 % (long int)val2); ///modulus
+
+                                operands.push(result);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        operation_error |= error::MISSING_OPERAND;
+                    }
+                }
+                else
+                {
+                    operation_error |= error::MISSING_OPERAND;
+                }
 
                 return operation_error;
             }

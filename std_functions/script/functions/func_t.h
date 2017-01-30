7477080624688b33a7cd1c422790238a5b0b5bc9
Zachary Westerman <zacharywesterman@yahoo.com>
2017-01-30 01:08:57 -0600

    cleared out dir

--- std_functions/script/functions/func_t.h
+++ std_functions/script/functions/func_t.h
@@ -2,6 +2,11 @@
 #ifndef FUNC_T_H_INCLUDED
 #define FUNC_T_H_INCLUDED
 
+#include "../../core/string.h"
+#include "../../core/array.h"
+
+#include "../script_errors.h"
+
 namespace script
 {
     namespace func
@@ -9,7 +14,28 @@ namespace script
         template <typename CHAR>
         class func_t
         {
+        private:
+            core::string<CHAR> name;
 
+            int min_params;
+            int max_params;
+
+        public:
+            func_t (const core::string<CHAR>& func_name,
+                    int min_p, int max_p)
+            {
+                name = func_name;
+
+                min_params = min_p;
+                max_params = max_p;
+            }
+
+
+            virtual error_flag call(const core::array< core::string<CHAR> >& params,
+                                    core::string<CHAR>& output)
+            {
+                return error::NONE;
+            }
         };
     }
 }

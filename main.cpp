172d781285ee4157c9859f1505b5148fce3bde0f
Zachary Westerman <zacharywesterman@yahoo.com>
2017-02-05 06:35:48 -0600

    moved load_operators funcs to own file

--- main.cpp
+++ main.cpp
@@ -1,117 +1,32 @@
-//#include "engine/game.h"
-
-//#include "core/sorted_array.h"
-
 #include "z/core/string.h"
 
-#include "z/script/preParser.h"
+//#include "z/script/preParser.h"
 
-#include "z/core/loadLibrary.h"
-
-#include <windows.h>
+#include "z/script/load_operators.h"
 
 #include <iostream>
 using namespace std;
 
-#include "script/operators/oper_t.h"
-#include "core/array.h"
-
-#include "file/list_files_in_dir.h"
-
-bool load_opers(core::array< script::oper::oper_t<char>* >& output, const core::string<char>& file_name)
-{
-    typedef void (__stdcall *_func) (core::array< script::oper::oper_t<char>* >&);
-
-    core::library::lib_t lib = NULL;
-    lib = core::library::load(file_name.str());
-
-
-    if (!lib)
-    {
-        return false;
-    }
-    else
-    {
-        _func get_opers = (_func)core::library::get_function(lib, "get_opers_c");
-
-        if (!get_opers)
-        {
-            return false;
-        }
-        else
-        {
-            get_opers(output);
-            return true;
-        }
-    }
-}
-
-
-bool load_opers(core::array< script::oper::oper_t<wchar_t>* >& output, const core::string<char>& file_name)
-{
-    typedef void (__stdcall *_func) (core::array< script::oper::oper_t<wchar_t>* >&);
-
-    core::library::lib_t lib = NULL;
-    lib = core::library::load(file_name.str());
-
-
-    if (!lib)
-    {
-        return false;
-    }
-    else
-    {
-        _func get_opers = (_func)core::library::get_function(lib, "get_opers_w");
-
-        if (!get_opers)
-        {
-            return false;
-        }
-        else
-        {
-            get_opers(output);
-            return true;
-        }
-    }
-}
-
-
-void load_all_opers(core::array< script::oper::oper_t<char>* >& output,
-                    const core::string<char>& folder_name)
-{
-    core::array< core::string<char> > files;
-
-    file::list_files_in_dir(folder_name, "dll", files);
-
-    for(int i=0; i<files.size(); i++)
-    {
-        core::string<char> file_name = folder_name;
-        file_name += '/';
-        file_name += files[i];
-
-        load_opers(output, file_name);
-    }
-}
 
 
 int main()
 {
     //load operators
-    core::array< script::oper::oper_t<char>* > operators;
+    z::core::array< z::script::oper::oper_t<char>* > operators;
 
-    load_all_opers(operators, "operators");
+    z::script::load_operators(operators, "operators");
 
 
 
     //test the pre-parser
-    script::preParser<char> preP (operators);
+    /*script::preParser<char> preP (operators);
 
     cout << "err[" << preP.split("print 10{\"arr[10]=\",arr[10]}") << "]\n";
 
     for (int i=0; i<preP.arr.size(); i++)
     {
         cout << preP.arr[i].str.str() << endl;
-    }
+    }*/
 
 
 

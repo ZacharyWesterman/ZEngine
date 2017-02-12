6d808f5bb061f797025c5c0d4b3aeed2876d2fe3
Zachary Westerman <zacharywesterman@yahoo.com>
2017-02-12 02:27:20 -0600

    output takes array pointer

--- std_operators/main.cpp
+++ std_operators/main.cpp
@@ -30,72 +30,78 @@
 #include "z/script/operators/not.h"
 
 
-__declspec(dllexport) void __cdecl get_opers_c(z::core::array< z::script::oper::oper_t<char>* >& output)
+__declspec(dllexport) void __cdecl get_opers_c(z::core::array< z::script::oper::oper_t<char>* >* output)
 {
-    //Addition
-    output.add(new z::script::oper::add<char>);
-    output.add(new z::script::oper::subtract<char>);
-    output.add(new z::script::oper::negate<char>);
+    if (output)
+    {
+        //Addition
+        output->add(new z::script::oper::add<char>);
+        output->add(new z::script::oper::subtract<char>);
+        output->add(new z::script::oper::negate<char>);
 
-    //Multiplication
-    output.add(new z::script::oper::multiply<char>);
-    output.add(new z::script::oper::divide<char>);
-    output.add(new z::script::oper::integer_divide<char>);
-    output.add(new z::script::oper::modulus<char>);
+        //Multiplication
+        output->add(new z::script::oper::multiply<char>);
+        output->add(new z::script::oper::divide<char>);
+        output->add(new z::script::oper::integer_divide<char>);
+        output->add(new z::script::oper::modulus<char>);
 
-    //Advanced operations
-    output.add(new z::script::oper::power<char>);
-    output.add(new z::script::oper::factorial<char>);
+        //Advanced operations
+        output->add(new z::script::oper::power<char>);
+        output->add(new z::script::oper::factorial<char>);
 
-    //Equality
-    output.add(new z::script::oper::equals<char>);
-    output.add(new z::script::oper::not_equal<char>);
-    output.add(new z::script::oper::greater_than<char>);
-    output.add(new z::script::oper::less_than<char>);
-    output.add(new z::script::oper::greater_than_equal<char>);
-    output.add(new z::script::oper::less_than_equal<char>);
+        //Equality
+        output->add(new z::script::oper::equals<char>);
+        output->add(new z::script::oper::not_equal<char>);
+        output->add(new z::script::oper::greater_than<char>);
+        output->add(new z::script::oper::less_than<char>);
+        output->add(new z::script::oper::greater_than_equal<char>);
+        output->add(new z::script::oper::less_than_equal<char>);
 
-    //Boolean logic
-    output.add(new z::script::oper::_and<char>);
-    output.add(new z::script::oper::_or<char>);
-    output.add(new z::script::oper::_nand<char>);
-    output.add(new z::script::oper::_nor<char>);
-    output.add(new z::script::oper::_xor<char>);
-    output.add(new z::script::oper::_xnor<char>);
-    output.add(new z::script::oper::_not<char>);
+        //Boolean logic
+        output->add(new z::script::oper::_and<char>);
+        output->add(new z::script::oper::_or<char>);
+        output->add(new z::script::oper::_nand<char>);
+        output->add(new z::script::oper::_nor<char>);
+        output->add(new z::script::oper::_xor<char>);
+        output->add(new z::script::oper::_xnor<char>);
+        output->add(new z::script::oper::_not<char>);
+    }
 }
 
-__declspec(dllexport) void __cdecl get_opers_w(z::core::array< z::script::oper::oper_t<wchar_t>* >& output)
+__declspec(dllexport) void __cdecl get_opers_w(z::core::array< z::script::oper::oper_t<wchar_t>* >* output)
 {
-    //Addition
-    output.add(new z::script::oper::add<wchar_t>);
-    output.add(new z::script::oper::subtract<wchar_t>);
-    output.add(new z::script::oper::negate<wchar_t>);
+    if (output)
+    {
+        //Addition
+        output->add(new z::script::oper::add<wchar_t>);
+        output->add(new z::script::oper::subtract<wchar_t>);
+        output->add(new z::script::oper::negate<wchar_t>);
 
-    //Multiplication
-    output.add(new z::script::oper::multiply<wchar_t>);
-    output.add(new z::script::oper::divide<wchar_t>);
-    output.add(new z::script::oper::integer_divide<wchar_t>);
-    output.add(new z::script::oper::modulus<wchar_t>);
+        //Multiplication
+        output->add(new z::script::oper::multiply<wchar_t>);
+        output->add(new z::script::oper::divide<wchar_t>);
+        output->add(new z::script::oper::integer_divide<wchar_t>);
+        output->add(new z::script::oper::modulus<wchar_t>);
 
-    //Advanced operations
-    output.add(new z::script::oper::power<wchar_t>);
-    output.add(new z::script::oper::factorial<wchar_t>);
+        //Advanced operations
+        output->add(new z::script::oper::power<wchar_t>);
+        output->add(new z::script::oper::factorial<wchar_t>);
 
-    //Equality
-    output.add(new z::script::oper::equals<wchar_t>);
-    output.add(new z::script::oper::not_equal<wchar_t>);
-    output.add(new z::script::oper::greater_than<wchar_t>);
-    output.add(new z::script::oper::less_than<wchar_t>);
-    output.add(new z::script::oper::greater_than_equal<wchar_t>);
-    output.add(new z::script::oper::less_than_equal<wchar_t>);
+        //Equality
+        output->add(new z::script::oper::equals<wchar_t>);
+        output->add(new z::script::oper::not_equal<wchar_t>);
+        output->add(new z::script::oper::greater_than<wchar_t>);
+        output->add(new z::script::oper::less_than<wchar_t>);
+        output->add(new z::script::oper::greater_than_equal<wchar_t>);
+        output->add(new z::script::oper::less_than_equal<wchar_t>);
 
-    //Boolean logic
-    output.add(new z::script::oper::_and<wchar_t>);
-    output.add(new z::script::oper::_or<wchar_t>);
-    output.add(new z::script::oper::_nand<wchar_t>);
-    output.add(new z::script::oper::_nor<wchar_t>);
-    output.add(new z::script::oper::_xor<wchar_t>);
-    output.add(new z::script::oper::_xnor<wchar_t>);
-    output.add(new z::script::oper::_not<wchar_t>);
+        //Boolean logic
+        output->add(new z::script::oper::_and<wchar_t>);
+        output->add(new z::script::oper::_or<wchar_t>);
+        output->add(new z::script::oper::_nand<wchar_t>);
+        output->add(new z::script::oper::_nor<wchar_t>);
+        output->add(new z::script::oper::_xor<wchar_t>);
+        output->add(new z::script::oper::_xnor<wchar_t>);
+        output->add(new z::script::oper::_not<wchar_t>);
+    }
 }
--- std_operators/main.h
+++ std_operators/main.h
@@ -5,8 +5,8 @@
 
 extern "C"
 {
-    __declspec(dllexport) void __cdecl get_opers_c(z::core::array< z::script::oper::oper_t<char>* >&);
+    __declspec(dllexport) void __cdecl get_opers_c(z::core::array< z::script::oper::oper_t<char>* >*);
 
-    __declspec(dllexport) void __cdecl get_opers_w(z::core::array< z::script::oper::oper_t<wchar_t>* >&);
+    __declspec(dllexport) void __cdecl get_opers_w(z::core::array< z::script::oper::oper_t<wchar_t>* >*);
 }
 

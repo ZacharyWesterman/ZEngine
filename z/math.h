3c2f219d691e23d0b871961de1947e2896a67f6b
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-03 12:53:14 -0600

    added <z/math.h> header file

---
+++ z/math.h
@@ -0,0 +1,26 @@
+/**
+ * File:            math.h
+ * Namespace:       none
+ * Description:     Header file which includes all
+ *                  z::math functions and constants.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   3 Mar. 2017
+**/
+
+
+#pragma once
+#ifndef MATH_H_INCLUDED
+#define MATH_H_INCLUDED
+
+///Functions
+#include "math/factorial.h"
+
+
+///Constants
+#include "math/constants.h"
+
+
+#endif // MATH_H_INCLUDED

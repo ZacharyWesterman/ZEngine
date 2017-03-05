fc9186927afc064fe78f8ebde4adf515dc07ca7e
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-05 16:37:45 -0600

    started lexer file

---
+++ z/script/parser/lexer.h
@@ -0,0 +1,33 @@
+/**
+ * File:            lexer.h
+ * Namespace:       z::script
+ * Description:     Script lexer template. The lexer
+ *                  takes the list of tokens generated
+ *                  by the scanner and groups them into
+ *                  functional sections (e.g. expression,
+ *                  scope, etc.)
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   5 Mar. 2017
+**/
+
+
+#pragma once
+#ifndef LEXER_H_INCLUDED
+#define LEXER_H_INCLUDED
+
+namespace z
+{
+    namespace script
+    {
+        template <typename CHAR>
+        class lexer
+        {
+
+        };
+    }
+}
+
+#endif // LEXER_H_INCLUDED

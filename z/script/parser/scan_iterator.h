ca7c26d1409e64a5090924c820b5023fb1ce48e7
Zachary Westerman <zacharywesterman@yahoo.com>
2017-03-12 16:33:22 -0500

    added constructor, destructor. Started scan() method.

--- z/script/parser/scan_iterator.h
+++ z/script/parser/scan_iterator.h
@@ -20,6 +20,7 @@
 #define SCAN_ITERATOR_H_INCLUDED
 
 #include <z/core/array.h>
+#include <z/file/loadFileToMemory.h>
 
 #include "scanner.h"
 
@@ -27,10 +28,95 @@ namespace z
 {
     namespace script
     {
+        template <typename CHAR>
         class scan_iterator
         {
+        private:
+            scanner<CHAR>* _scanner;
 
+            bool found_error;
+
+        public:
+            core::array< ident_t<CHAR> > identifiers;
+
+
+            scan_iterator(core::sorted_array< core::string<CHAR> >* opers = NULL,
+                          core::sorted_array< core::string<CHAR> >* cmds = NULL,
+                          core::sorted_array< core::string<CHAR> >* funcs = NULL)
+            {
+                _scanner = new scanner<CHAR>(opers, cmds, funcs);
+
+                found_error = false;
+            }
+
+            ~scan_iterator()
+            {
+                delete _scanner;
+            }
+
+
+            void set(const core::string<CHAR>&);
+
+
+            bool scan();
+
+            bool error();
         };
+
+
+        ///set the input string for the scan iterator
+        ///and scan that input (does not replace includes).
+        template <typename CHAR>
+        void scan_iterator<CHAR>::set(const core::string<CHAR>& input)
+        {
+            identifiers.clear();
+
+            found_error = _scanner->scan(input);
+            found_error |= _scanner->clean();
+
+            identifiers = _scanner->identifiers;
+        }
+
+
+        ///template for iterator scanner. Scans the loaded
+        ///input and replaces all includes in the script
+        ///with the contents of the referenced file.
+        //returns false if done scanning or unable to load
+        //an included file, true otherwise.
+        template <typename CHAR>
+        bool scan_iterator<CHAR>::scan()
+        {
+            for (int i=0; i<identifiers.size(); i++)
+                if (identifiers[i].type == ident::KEYWORD_INCLUDE)
+                {
+                    //include directories must exist and be a string literal.
+                    if (identifiers.is_valid(i+1) &&
+                        identifiers[i+1].type == ident::STRING_LITERAL)
+                    {
+                        core::string<CHAR> include_script;
+
+                        //if the include file exists, load it.
+                        if (false && file::loadFileToMemory(identifiers[i+1].name, include_script))
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+
+                    }
+                    else
+                    {
+                        found_error = true;
+
+                        identifiers[i].err = error::INVALID_INCLUDE;
+                    }
+
+                }
+
+            return false;
+        }
     }
 }
 

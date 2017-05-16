50bbff18cb71deeffa53a5eae9e7571013953003
Unknown <zacharywesterman@yahoo.com>
2017-05-16 01:31:59 -0500

    added line, column and error

--- z/script/parser/phrase.h
+++ z/script/parser/phrase.h
@@ -42,17 +42,34 @@ namespace z
         public:
             phrase::phrase_enum type;
 
-            int ident_begin;
-            int ident_end;
+            int line_begin;
+            int column_begin;
+
+            int line_end;
+            int column_end;
+
+            error_flag err;
+
+            ident_t<CHAR> data;
 
             core::array<phrase_t> children;
 
-            phrase_t()
+            phrase_t(phrase::phrase_enum p_type = phrase::NONE)
             {
-                ident_begin = -1;
-                ident_end = -1;
+                type = p_type;
 
-                type = phrase::NONE;
+                line_begin = -1;
+                column_begin = -1;
+
+                line_end = -1;
+                column_end = -1;
+
+                err = error::NONE;
+            }
+
+            inline bool operator==(const phrase_t& other) const
+            {
+                return (type == other.type) && (data == other.data);
             }
         };
     }

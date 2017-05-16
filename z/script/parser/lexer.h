394a6599a923c373ceeafbc99589b312b6831acc
Unknown <zacharywesterman@yahoo.com>
2017-05-16 00:48:09 -0500

    started work on lexer

--- z/script/parser/lexer.h
+++ z/script/parser/lexer.h
@@ -18,6 +18,8 @@
 #ifndef LEXER_H_INCLUDED
 #define LEXER_H_INCLUDED
 
+#include <z/core/timeout.h>
+
 #include "phrase.h"
 
 namespace z
@@ -27,8 +29,57 @@ namespace z
         template <typename CHAR>
         class lexer
         {
+        private:
+            core::array< ident_t<CHAR> >* input_ident;
 
+            core::array< phrase<CHAR> > syntax_tree;
+
+            bool initial_scan;
+
+            int current_ident;
+            int current_index;
+
+        public:
+            lexer()
+            {
+                input_ident = NULL;
+                initial_scan = true;
+
+                current_ident = -1;
+                current_index = -1;
+            }
+
+            void setInput(core::array< ident_t<CHAR> >& identifiers)
+            {
+                input_ident = &identifiers;
+                syntax_tree.clear();
+
+                initial_scan = true;
+            }
+
+            bool lex(const core::timeout&);
         };
+
+
+        template <typename CHAR>
+        bool lexer<CHAR>::lex(const core::timeout& time)
+        {
+            while (!time.timedOut())
+            {
+                if (initial_scan)
+                {
+                    if ()
+
+                    initial_scan = false;
+                }
+                else
+                {
+
+                }
+            }
+
+            return !time.timedOut();
+        }
     }
 }
 

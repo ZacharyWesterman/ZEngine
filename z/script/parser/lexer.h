77fa11f1a1d009a742322d6fc98ff5eeb0f3428f
Unknown <zacharywesterman@yahoo.com>
2017-05-29 00:24:17 -0500

    lexer now concatenates multiplcation and addition expressions

--- z/script/parser/lexer.h
+++ z/script/parser/lexer.h
@@ -74,8 +74,7 @@ namespace z
             enum progress
             {
                 NONE = 0,
-                GET_ADD_EXPRS,
-                GET_MUL_EXPRS,
+                LEXICALS,
                 DONE
             };
         }
@@ -87,16 +86,38 @@ namespace z
         private:
             core::array< ident_t<CHAR> >* input_ident;
 
-            core::array< phrase_t<CHAR> > phrase_list;
+            core::array< phrase_t<CHAR>* > phrase_nodes;
+
+            phrase_t<CHAR>* current_node;
 
             int index;
             bool found_error;
 
             lex::progress progress;
+            bool input_in_use;
 
             ///debug
             void print_lex_ast();
             void print_lx_ch(int, phrase_t<CHAR>*);
+
+            void del_nodes(phrase_t<CHAR>* root)
+            {
+                for (int i=0; i<(root->children.size()); i++)
+                {
+                    del_nodes(root->children[i]);
+                }
+
+                delete root;
+            }
+
+
+            ///phrase detection
+            bool found_mul_expr();
+            bool found_add_expr();
+
+            ///phrase concatenation
+            void concat_mul_expr();
+            void concat_add_expr();
 
         public:
             lexer()
@@ -106,12 +127,23 @@ namespace z
 
                 index = 0;
                 found_error = false;
+
+                input_in_use = true;
+            }
+
+            ~lexer()
+            {
+                for (int i=0; i<phrase_nodes.size(); i++)
+                    del_nodes(phrase_nodes[i]);
             }
 
             void setInput(core::array< ident_t<CHAR> >& identifiers)
             {
                 input_ident = &identifiers;
-                phrase_list.clear();
+
+                for (int i=0; i<phrase_nodes.size(); i++)
+                    del_nodes(phrase_nodes[i]);
+                phrase_nodes.clear();
 
                 progress = lex::NONE;
 
@@ -119,9 +151,12 @@ namespace z
                 found_error = false;
             }
 
+
             bool lex(const core::timeout&);
 
             inline bool error() {return found_error;}
+
+            inline bool usingInput() {return input_in_use;}
         };
 
 
@@ -132,36 +167,31 @@ namespace z
             {
                 if (progress == lex::NONE)
                 {
-                    phrase_list.add(phrase_t<CHAR>());
-                    phrase_list[index].data = input_ident->at(index);
+                    phrase_nodes.add(new phrase_t<CHAR>(input_ident->at(index)));
 
                     index++;
 
                     if (index >= input_ident->size())
                     {
-                        progress = lex::GET_MUL_EXPRS;
+                        progress = lex::LEXICALS;
                         index = 0;
+
+                        input_in_use = false;
                     }
                 }
-                else if (progress == lex::GET_MUL_EXPRS)
+                else if (progress == lex::LEXICALS)
                 {
-                    index++;
-
-                    if (index >= phrase_list.size())
-                    {
-                        progress = lex::GET_ADD_EXPRS;
-                        index = 0;
-                    }
-                }
-                else if (progress == lex::GET_ADD_EXPRS)
-                {
-                    index++;
-
-                    if (index >= phrase_list.size())
+                    if (index >= phrase_nodes.size())
                     {
                         progress = lex::DONE;
                         index = 0;
                     }
+                    else if (found_mul_expr())
+                        concat_mul_expr();
+                    else if (found_add_expr())
+                        concat_add_expr();
+                    else
+                        index++;
                 }
             }
 
@@ -171,6 +201,86 @@ namespace z
 
             return !time.timedOut();
         }
+
+
+
+        ///phrase detection
+
+        template <typename CHAR>
+        bool lexer<CHAR>::found_mul_expr()
+        {
+            if ((index+2) >= phrase_nodes.size())
+                return false;
+            else
+                return (phrase_nodes[index+1]->type == ident::OPER_MUL) ||
+                       (phrase_nodes[index+1]->type == ident::OPER_DIV) ||
+                       (phrase_nodes[index+1]->type == ident::OPER_IDIV) ||
+                       (phrase_nodes[index+1]->type == ident::OPER_MOD);
+        }
+
+        template <typename CHAR>
+        bool lexer<CHAR>::found_add_expr()
+        {
+            if ((index+2) >= phrase_nodes.size())
+                return false;
+            else
+                return (phrase_nodes[index+1]->type == ident::OPER_ADD) ||
+                       (phrase_nodes[index+1]->type == ident::OPER_SUB);
+        }
+
+
+        ///phrase concatenation
+
+        template <typename CHAR>
+        void lexer<CHAR>::concat_mul_expr()
+        {
+            phrase_t<CHAR>* mul_node = new phrase_t<CHAR>();
+
+            mul_node->type = phrase::MUL_EXPR;
+
+            mul_node->line = phrase_nodes[index]->line;
+            mul_node->column = phrase_nodes[index]->column;
+
+            phrase_nodes[index]->parent = mul_node;
+            phrase_nodes[index+1]->parent = mul_node;
+            phrase_nodes[index+2]->parent = mul_node;
+
+            mul_node->children.add(phrase_nodes[index]);
+            mul_node->children.add(phrase_nodes[index+1]);
+            mul_node->children.add(phrase_nodes[index+2]);
+
+            phrase_nodes.replace(index, index+2, mul_node);
+
+            mul_node->err = error::NONE;
+
+            index = 0;
+        }
+
+        template <typename CHAR>
+        void lexer<CHAR>::concat_add_expr()
+        {
+            phrase_t<CHAR>* add_node = new phrase_t<CHAR>();
+
+            add_node->type = phrase::ADD_EXPR;
+
+            add_node->line = phrase_nodes[index]->line;
+            add_node->column = phrase_nodes[index]->column;
+
+            phrase_nodes[index]->parent = add_node;
+            phrase_nodes[index+1]->parent = add_node;
+            phrase_nodes[index+2]->parent = add_node;
+
+            add_node->children.add(phrase_nodes[index]);
+            add_node->children.add(phrase_nodes[index+1]);
+            add_node->children.add(phrase_nodes[index+2]);
+
+            phrase_nodes.replace(index, index+2, add_node);
+
+            add_node->err = error::NONE;
+
+            index = 0;
+        }
+
 
 
         ///debug

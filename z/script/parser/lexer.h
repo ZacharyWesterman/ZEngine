d1d5144aa158d92da4e2e5fa4e2e59ee6b1556eb
Unknown <zacharywesterman@yahoo.com>
2017-06-19 13:20:53 -0500

    implemented "wait" and "wait until" statement syntax.

--- z/script/parser/lexer.h
+++ z/script/parser/lexer.h
@@ -10,7 +10,7 @@
  *
  * Author:          Zachary Westerman
  * Email:           zacharywesterman@yahoo.com
- * Last modified:   12 Jun. 2017
+ * Last modified:   19 Jun. 2017
 **/
 
 
@@ -316,6 +316,8 @@ namespace z
                              gosub_statement()  ||
                              run_statement()    ||
                              return_statement() ||
+                             wait_statement()   ||
+                             until_statement()  ||
 
                              subroutine_decl()  ||
                              variable_decl()    ||
@@ -1553,6 +1555,67 @@ namespace z
             else
                 return false;
         }
+
+        template <typename CHAR>
+        bool lexer<CHAR>::wait_statement()
+        {
+            if (phrase_nodes.is_valid(index+2) &&
+                (phrase_nodes[index]->type == ident::KEYWORD_WAIT) &&
+                (phrase_nodes[index+1]->type == phrase::BOOLEXPR) &&
+                (phrase_nodes[index+2]->type == ident::SEMICOLON))
+            {
+                phrase_t<CHAR>* node = new phrase_t<CHAR>();
+
+                node->type = phrase::WAIT_STATEMENT;
+
+                node->line = phrase_nodes[index]->line;
+                node->column = phrase_nodes[index]->column;
+
+                phrase_nodes[index+1]->parent = node;
+
+                node->children.add(phrase_nodes[index+1]);
+
+                delete phrase_nodes[index];
+                delete phrase_nodes[index+2];
+                phrase_nodes.replace(index, index+2, node);
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        template <typename CHAR>
+        bool lexer<CHAR>::until_statement()
+        {
+            if (phrase_nodes.is_valid(index+3) &&
+                (phrase_nodes[index]->type == ident::KEYWORD_WAIT) &&
+                (phrase_nodes[index+1]->type == ident::KEYWORD_UNTIL) &&
+                (phrase_nodes[index+2]->type == phrase::BOOLEXPR) &&
+                (phrase_nodes[index+3]->type == ident::SEMICOLON))
+            {
+                phrase_t<CHAR>* node = new phrase_t<CHAR>();
+
+                node->type = phrase::UNTIL_STATEMENT;
+
+                node->line = phrase_nodes[index]->line;
+                node->column = phrase_nodes[index]->column;
+
+                phrase_nodes[index+2]->parent = node;
+
+                node->children.add(phrase_nodes[index+2]);
+
+                delete phrase_nodes[index];
+                delete phrase_nodes[index+1];
+                delete phrase_nodes[index+3];
+                phrase_nodes.replace(index, index+3, node);
+
+                return true;
+            }
+            else
+                return false;
+        }
+
 
         template <typename CHAR>
         bool lexer<CHAR>::subroutine_decl()

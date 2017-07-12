2f6e0d6417377ef7023cec89c966afdb711dda18
Unknown <zacharywesterman@yahoo.com>
2017-07-12 16:04:48 -0500

    implemented function declaration syntax

--- z/script/parser/lexer.h
+++ z/script/parser/lexer.h
@@ -306,8 +306,8 @@ namespace z
                     {
                         if (!did_concat)
                         {
-                            progress = lex::TREE_CLEANUP;
-                            //progress = lex::DONE;
+                            //progress = lex::TREE_CLEANUP;
+                            progress = lex::DONE;
                             current_node = 0;
                         }
 
@@ -317,6 +317,7 @@ namespace z
                     else if (formalvardecl()    ||
                              formaltypedecl()   ||
                              formaldecllist()   ||
+                             function_decl()    ||
 
                              externaldecl()     ||
                              shareddecl()       ||
@@ -3004,6 +3005,147 @@ namespace z
             else
                 return false;
         }
+
+        template <typename CHAR>
+        bool lexer<CHAR>::function_decl()
+        {
+            if (phrase_nodes.is_valid(index+5) &&
+                (phrase_nodes[index]->type == ident::KEYWORD_FUNCTION) &&
+                (phrase_nodes[index+1]->type == ident::IDENTIFIER) &&
+                (phrase_nodes[index+2]->type == ident::LPARENTH))
+            {
+
+                if (phrase_nodes.is_valid(index+7) &&
+                    ((phrase_nodes[index+3]->type == phrase::FORMALVARDECL) ||
+                     (phrase_nodes[index+3]->type == phrase::FORMALTYPEDECL) ||
+                     (phrase_nodes[index+3]->type == phrase::FORMALDECLLIST)) &&
+                    (phrase_nodes[index+4]->type == ident::RPARENTH) &&
+                    (phrase_nodes[index+5]->type == ident::LBRACE) &&
+                    ((phrase_nodes[index+6]->type == phrase::STATEMENTLIST) ||
+                     (phrase_nodes[index+6]->type == phrase::STATEMENT)) &&
+                    (phrase_nodes[index+7]->type == ident::RBRACE))
+                {
+                    phrase_t<CHAR>* node = new phrase_t<CHAR>();
+
+                    node->type = phrase::FUNCTION_DECL;
+
+                    node->line = phrase_nodes[index]->line;
+                    node->column = phrase_nodes[index]->column;
+
+                    phrase_nodes[index+1]->parent = node;
+                    phrase_nodes[index+3]->parent = node;
+                    phrase_nodes[index+6]->parent = node;
+
+                    node->children.add(phrase_nodes[index+1]);
+                    node->children.add(phrase_nodes[index+3]);
+                    node->children.add(phrase_nodes[index+6]);
+
+                    node->file = phrase_nodes[index]->file;
+
+                    delete phrase_nodes[index];
+                    delete phrase_nodes[index+2];
+                    delete phrase_nodes[index+4];
+                    delete phrase_nodes[index+5];
+                    delete phrase_nodes[index+7];
+                    phrase_nodes.replace(index, index+7, node);
+
+                    return true;
+                }
+                else if (phrase_nodes.is_valid(index+6))
+                {
+                    if (((phrase_nodes[index+3]->type == phrase::FORMALVARDECL) ||
+                         (phrase_nodes[index+3]->type == phrase::FORMALTYPEDECL) ||
+                         (phrase_nodes[index+3]->type == phrase::FORMALDECLLIST)) &&
+                        (phrase_nodes[index+4]->type == ident::RPARENTH) &&
+                        (phrase_nodes[index+5]->type == ident::LBRACE) &&
+                        (phrase_nodes[index+6]->type == ident::RBRACE))
+                    {
+                        phrase_t<CHAR>* node = new phrase_t<CHAR>();
+
+                        node->type = phrase::FUNCTION_DECL;
+
+                        node->line = phrase_nodes[index]->line;
+                        node->column = phrase_nodes[index]->column;
+
+                        phrase_nodes[index+1]->parent = node;
+                        phrase_nodes[index+3]->parent = node;
+
+                        node->children.add(phrase_nodes[index+1]);
+                        node->children.add(phrase_nodes[index+3]);
+
+                        node->file = phrase_nodes[index]->file;
+
+                        delete phrase_nodes[index];
+                        delete phrase_nodes[index+2];
+                        delete phrase_nodes[index+4];
+                        delete phrase_nodes[index+5];
+                        delete phrase_nodes[index+6];
+                        phrase_nodes.replace(index, index+6, node);
+
+                        return true;
+                    }
+                    else if ((phrase_nodes[index+3]->type == ident::RPARENTH) &&
+                             (phrase_nodes[index+4]->type == ident::LBRACE) &&
+                             ((phrase_nodes[index+5]->type == phrase::STATEMENTLIST) ||
+                              (phrase_nodes[index+5]->type == phrase::STATEMENT)) &&
+                             (phrase_nodes[index+6]->type == ident::RBRACE))
+                    {
+                        phrase_t<CHAR>* node = new phrase_t<CHAR>();
+
+                        node->type = phrase::FUNCTION_DECL;
+
+                        node->line = phrase_nodes[index]->line;
+                        node->column = phrase_nodes[index]->column;
+
+                        phrase_nodes[index+1]->parent = node;
+                        phrase_nodes[index+5]->parent = node;
+
+                        node->children.add(phrase_nodes[index+1]);
+                        node->children.add(phrase_nodes[index+5]);
+
+                        node->file = phrase_nodes[index]->file;
+
+                        delete phrase_nodes[index];
+                        delete phrase_nodes[index+2];
+                        delete phrase_nodes[index+3];
+                        delete phrase_nodes[index+4];
+                        delete phrase_nodes[index+6];
+                        phrase_nodes.replace(index, index+6, node);
+
+                        return true;
+                    }
+                }
+                else if ((phrase_nodes[index+3]->type == ident::RPARENTH) &&
+                         (phrase_nodes[index+4]->type == ident::LBRACE) &&
+                         (phrase_nodes[index+5]->type == ident::RBRACE))
+                {
+                    phrase_t<CHAR>* node = new phrase_t<CHAR>();
+
+                    node->type = phrase::FUNCTION_DECL;
+
+                    node->line = phrase_nodes[index]->line;
+                    node->column = phrase_nodes[index]->column;
+
+                    phrase_nodes[index+1]->parent = node;
+
+                    node->children.add(phrase_nodes[index+1]);
+
+                    node->file = phrase_nodes[index]->file;
+
+                    delete phrase_nodes[index];
+                    delete phrase_nodes[index+2];
+                    delete phrase_nodes[index+3];
+                    delete phrase_nodes[index+4];
+                    delete phrase_nodes[index+5];
+                    phrase_nodes.replace(index, index+5, node);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
 
 
         ///debug

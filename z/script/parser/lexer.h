5c243ec1c3b6000727714a3008a7422ced159a6b
Unknown <zacharywesterman@yahoo.com>
2017-07-12 17:48:51 -0500

    implemented type declaration syntax

--- z/script/parser/lexer.h
+++ z/script/parser/lexer.h
@@ -308,8 +308,8 @@ namespace z
                     {
                         if (!did_concat)
                         {
-                            //progress = lex::TREE_CLEANUP;
-                            progress = lex::DONE;
+                            progress = lex::TREE_CLEANUP;
+                            //progress = lex::DONE;
                             current_node = 0;
                         }
 
@@ -321,6 +321,7 @@ namespace z
                              formaldecllist()   ||
                              function_decl()    ||
                              int_decllist()     ||
+                             typedecl()         ||
 
                              externaldecl()     ||
                              shareddecl()       ||
@@ -3186,6 +3187,45 @@ namespace z
                 node->file = phrase_nodes[index-1]->file;
 
                 phrase_nodes.replace(index-1, index, node);
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        template <typename CHAR>
+        bool lexer<CHAR>::typedecl()
+        {
+            if ((phrase_nodes.is_valid(index+4) &&
+                 (phrase_nodes[index]->type == ident::KEYWORD_TYPE) &&
+                 (phrase_nodes[index+1]->type == ident::IDENTIFIER) &&
+                 (phrase_nodes[index+2]->type == ident::LBRACE)) &&
+                 ((phrase_nodes[index+3]->type == phrase::VARIABLE_DECL) ||
+                  (phrase_nodes[index+3]->type == phrase::TYPEVAR_DECL) ||
+                  (phrase_nodes[index+3]->type == phrase::FUNCTION_DECL) ||
+                  (phrase_nodes[index+3]->type == phrase::INT_DECLLIST)) &&
+                (phrase_nodes[index+4]->type == ident::RBRACE))
+            {
+                phrase_t<CHAR>* node = new phrase_t<CHAR>();
+
+                node->type = phrase::TYPEDECL;
+
+                node->line = phrase_nodes[index]->line;
+                node->column = phrase_nodes[index]->column;
+
+                phrase_nodes[index+1]->parent = node;
+                phrase_nodes[index+3]->parent = node;
+
+                node->children.add(phrase_nodes[index+1]);
+                node->children.add(phrase_nodes[index+3]);
+
+                node->file = phrase_nodes[index]->file;
+
+                delete phrase_nodes[index];
+                delete phrase_nodes[index+2];
+                delete phrase_nodes[index+4];
+                phrase_nodes.replace(index, index+4, node);
 
                 return true;
             }

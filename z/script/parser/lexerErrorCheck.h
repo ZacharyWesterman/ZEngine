10dc255bacd42b5b26f8f02e07567cdafc8b6e99
Unknown <zacharywesterman@yahoo.com>
2017-07-12 22:42:31 -0500

    created file for future syntax error checking implementation

---
+++ z/script/parser/lexerErrorCheck.h
@@ -0,0 +1,29 @@
+/**
+ * File:            lexerErrorCheck.h
+ * Namespace:       z::script
+ * Description:     Implementation of the lexer's
+ *                  error checking member functions.
+ *                  This file should NOT be included
+ *                  in a project. Include "lexer.h"
+ *                  instead,and put this file in the
+ *                  same folder.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   12 Jul. 2017
+**/
+
+#pragma once
+#ifndef LEXERERRORCHECK_H_INCLUDED
+#define LEXERERRORCHECK_H_INCLUDED
+
+namespace z
+{
+    namespace script
+    {
+
+    }
+}
+
+#endif // LEXERERRORCHECK_H_INCLUDED

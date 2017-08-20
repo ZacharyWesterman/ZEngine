3be0a72778db5cccf6fe202a684f43ce668451ef
Unknown <zacharywesterman@yahoo.com>
2017-08-20 15:10:27 -0500

    started file for semantic analyser

---
+++ z/script/parser/semantic.h
@@ -0,0 +1,32 @@
+/**
+ * File:            lexer.h
+ * Namespace:       z::script
+ * Description:     Semantic analyser template. Checks
+ *                  for invalid semantics in the given
+ *                  syntax tree. Additionally, indexes
+ *                  variables, functions, types, and
+ *                  commands for easy reference later on.
+ *
+ *
+ * Author:          Zachary Westerman
+ * Email:           zacharywesterman@yahoo.com
+ * Last modified:   20 Aug. 2017
+**/
+
+#pragma once
+#ifndef SEMANTIC_H_INCLUDED
+#define SEMANTIC_H_INCLUDED
+
+namespace z
+{
+    namespace script
+    {
+        template <typename CHAR>
+        class semantic
+        {
+
+        };
+    }
+}
+
+#endif // SEMANTIC_H_INCLUDED

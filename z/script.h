93752f0443e39c7d7dd9e2af7f3c15f3f2c69326
Unknown <zacharywesterman@yahoo.com>
2017-09-08 23:22:36 -0500

    added file for script namespace

---
+++ z/script.h
@@ -0,0 +1,27 @@
+/**
+ * \file z/script.h
+ * \namespace z::script
+ * \brief Library containing all script-related
+ * functions and classes.
+ *
+ * This library adds advanced functionality
+ * including generic data types and the script
+ * compiler.
+**/
+
+#pragma once
+#ifndef SCRIPT_H_INCLUDED
+#define SCRIPT_H_INCLUDED
+
+///Basic supporting files
+#include "script/errors.h"
+#include "script/data_t.h"
+
+///Extensible script parts
+#include "script/function_t.h"
+#include "script/command_t.h"
+
+///
+#include "script/compiler.h"
+
+#endif // SCRIPT_H_INCLUDED
